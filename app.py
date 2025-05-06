9c06ca0eed8b7f5986a742d2afb7ce8a7be0155f
ahmedelhefnawy <ahmedelhefnawy2003@yahoo.com>
2025-05-06 08:20:21 +0300

    Added favicon &  Refined Output

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Image Caption Generator", page_icon=":camera:")
+st.set_page_config(page_title="Image Caption Generator", page_icon="https://cdn-icons-png.flaticon.com/512/18561/18561814.png")
 
 if 'preprocess_image' not in st.session_state:
     with st.spinner("Loading Preprocessing Module..."):
@@ -23,23 +23,34 @@ tokenizer = st.session_state.tokenizer
 preprocess_image = st.session_state.preprocess_image
 generate_caption = st.session_state.generate_caption
 
-st.title("Vision Transformer Based Image Caption Generator")
-st.markdown("## Upload an Image to Generate a Caption")
-
+st.title("ViT Image Caption Generator")
+st.markdown(
+    "This application generates captions for images using a Vision Transformer model.<br>"
+    "Upload an image and click the button to generate a caption.",
+    unsafe_allow_html=True
+)
 file = st.file_uploader("Upload File", accept_multiple_files=False)
-generate_button = st.button("Generate Caption")
+generate_button = st.button("Generate Caption", use_container_width=True)
 
 if file:
     st.markdown("## Loaded Image")
     st.image(file, use_container_width=False, width=400)
 
+
 if generate_button:
     if not file:
         st.error("Upload an image to generate caption")
-        st.stop()
     else:
         with st.spinner("Generating Caption ..."):
             preprocessed_image = preprocess_image(file)
             caption = generate_caption(caption_model, preprocessed_image, tokenizer)
 
-        st.markdown(f"### {caption.capitalize()}")
+        st.markdown(
+            f"""
+            <div style="text-align: center; margin-top: 20px;">
+            <h3 style="color: #4CAF50;">Generated Caption:</h3>
+            <p style="font-size: 18px;">{caption.capitalize()}</p>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
